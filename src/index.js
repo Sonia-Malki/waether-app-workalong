3362173cc14a163ecb86ee2c58132d837153e344
Sonia Malki <soniamalki22@gmail.com>
2024-05-03 15:55:53 +0200

    add icon api data

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ function refreshWeather(response) {
   // Time
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
-  console.log(response.data);
+  // Icon
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -71,9 +72,3 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 // Default search
 searchCity("Paris");
-
-//
-
-// Humidity and Wind
-
-// Current Date and Time
